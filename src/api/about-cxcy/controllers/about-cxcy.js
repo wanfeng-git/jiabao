c522dd51da9ce27cc3e401267e7540919f69a4d1
jin.qin <jin.qin@hicity.world>
2025-05-08 15:23:09 +0800

    commit

---
+++ src/api/about-cxcy/controllers/about-cxcy.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * about-cxcy controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::about-cxcy.about-cxcy');
